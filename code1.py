81c4efc89f9825b9e08041830e4d2c82ac215bb2
AhmadBilal07 <ahmadbilal07@live.com>
2021-01-23 00:55:33 +0100

    Laplacian Edge Detection without openCv

---
+++ code1.py
@@ -0,0 +1,54 @@
+import numpy as np
+from PIL import Image,ImageOps,ImageFilter
+
+# Convolutional Function
+def convolution(image, kernel):
+    m, n = kernel.shape
+    if (m == n):
+        x, y = image.shape
+        x = x - m + 1
+        y = y - m + 1
+        resultImg = np.zeros((x,y))
+        for i in range(y):
+            for j in range(x):
+                resultImg[i][j] = np.sum(image[i:i+m, j:j+m]*kernel)
+    return resultImg
+
+
+if __name__ == '__main__':
+    # Image Path
+    IMAGE = "sample images/lena.png"
+
+    # Opening Image using PIL
+    img = Image.open(IMAGE)
+
+    #Removing Noise & smoothing image for better performance
+    img = img.filter(ImageFilter.SMOOTH)
+
+    # Converting RGB to grayscale image
+    grayImg = ImageOps.grayscale(img)
+
+    # Converting grayscale img into array
+    imgArr = np.asarray(grayImg, dtype="int32")
+
+    # Padding Image with zeros
+    imgArr = np.pad(imgArr, 1, mode='constant')
+
+    '''
+    3x3 Laplacian Mask
+    '''
+
+    kernel = np.array([
+        [-1,-1,-1],
+        [-1,8,-1],
+        [-1,-1,-1]])
+
+    # Applying Convolution using our kernel Function
+    dstImg = convolution(imgArr, kernel)
+
+    # Converting array to back to image
+    dstImg = Image.fromarray(dstImg)
+
+    # Displaying both original and resultant images
+    grayImg.show()
+    dstImg.show()
